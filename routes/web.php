550982eaaba669b6a479164b000700c784f9a8ec
R4hm4tH <rahmatrahmat2307@gmail.com>
2025-04-26 22:58:51 +0700

    Update fitur X atau perbaikan Y

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Services\ApiService;
 
-// Route for returning a JSON response for the API
-Route::get('api/teams', function () {
-    return response()->json(['message' => 'Welcome to the API (Rahmat Hidayat)']);
+// Route homepage sederhana
+Route::get('api/rapidapi.com', function () {
+    return 'Selamat datang di halaman utama! Salam dari Rahmat Hidayat';
 });
 
-// Route for fetching premier league teams
-Route::get('api/premier-league/teams', [ApiService::class, 'getTeams']);
+// Route dengan parameter wajib
+Route::get('/user/{id}', function ($id) {
+    return "Menampilkan data user dengan ID: $id";
+});
+
+// Route POST sederhana untuk menerima data
+Route::post('/submit', function () {
+    return 'Data berhasil dikirim!';
+});
